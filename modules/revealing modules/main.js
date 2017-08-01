f4954861a0d01cec9f109f0a77a528710e9fd73e
michael <mmansourati@gmail.com>
2017-08-01 16:41:39 -0400

    revealing modules

---
+++ modules/revealing modules/main.js
@@ -0,0 +1,7 @@
+const mrm = require( './myRevealingModule' );
+
+mrm.myRevealingModule.setName( "name mcTesterson" );
+
+console.log(mrm.myRevealingModule.greeting);
+
+mrm.myRevealingModule.getName();
---
+++ modules/revealing modules/myRevealingModule.js
@@ -0,0 +1,29 @@
+exports.myRevealingModule = (function () {
+
+  let privateVar = "Ben Cherry",
+      publicVar = "Hey There!";
+
+  function privateFunction () {
+    console.log("Name: " + privateVar);
+  }
+
+  function publicSetName( strName ) {
+    privateVar = strName;
+  }
+
+  function publicGetName() {
+    privateFunction();
+  }
+
+  // Reveal public pointers to
+  // private functions and properties
+
+  return {
+    setName: publicSetName,
+    greeting: publicVar,
+    getName: publicGetName
+  };
+
+}) ();
+
+exports.myRevealingModule.setName( "Paul Kinlan" );
